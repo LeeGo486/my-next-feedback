cf1342a0400a2b56d87892cfdf6bdca4626dcf07
Rick Lau <liuh01.cn@gmail.com>
2023-07-12 17:21:02 +0800

    fix 第五次尝试，增加了表情包

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,64 @@
 'use client'
 import {motion} from "framer-motion"
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import './page.css'
 
+const emojis = ['🥰', '😄', '🤨', '😰']
 
-const variants = {
+
+const variantsLayout = {
   active: {
-    width: '40rem',
+    width: '25rem',
     height: '18rem',
     borderRadius: '1rem',
   },
   inactive: {
-    width: "35rem",
-    height: "4rem",
+    width: "21rem",
+    height: "3rem",
     borderRadius: "2rem",
   }
 }
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
-  const handleExpend = () => {
-    setIsOpen(isOpen => !isOpen)
+  const [selected, setSelected] = useState(-1);
+
+  useEffect(()=>{
+
+  },[isOpen, selected])
+  const handleExpend = (index: number) => {
+
+    setSelected(index)
+
+    if (index === selected) {
+      setIsOpen(isOpen => !isOpen)
+    } else {
+      setIsOpen(true)
+    }
   }
 
   return (
     <main className="flex flex-col min-h-screen items-center p-24">
-      <motion.div className="border border-slate-400 text-xl "
-                  onClick={() => handleExpend()}
-                  initial={false} variants={variants}
+      <motion.div className="border border-slate-400 text-base"
+                  initial={false} variants={variantsLayout}
                   animate={isOpen ? "active" : "inactive"}
                   transition={{
-                    type: "spring",
-                    duration: .4,
-                    bounce: 0.35,
+                    type: "tween",
+                    ease: "linear",
+                    duration: .1,
                   }}
       >
-        <div>111</div>
+        <div className="w-full h-12 px-4 py-2 gap-2 flex items-center justify-center">
+          <p>Was this helpful?</p> <span className="flex items-center justify-center gap-px">
+            {
+              emojis.map((emoji, index) => (
+                <button className="emojiButton" key={index} aria-checked={index === selected}
+                        onClick={() => handleExpend(index)}>{emoji}</button>
+              ))
+            }
+          </span>
+        </div>
+        <div className={"p-5" + (isOpen ? "" : " hidden")}>123</div>
       </motion.div>
     </main>
   )

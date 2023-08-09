1428a12fcf8540f4284cd225d10a86bd076c4efc
Rick Lau <liuh01.cn@gmail.com>
2023-08-09 10:14:44 +0800

    fix 调整整体布局，使之符合手机屏幕

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Feedback from './feedback/page'
 export default function Home() {
 
   return (
-    <main className="flex flex-col min-h-screen items-center p-24">
-      <Feedback/>
+    <main className="flex flex-col min-h-screen items-center">
+      {/*<Feedback/>*/}
     </main>
   )
 }

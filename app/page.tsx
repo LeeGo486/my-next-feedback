256823faad3aa3c1d599d86087fc2a19357955e2
Rick Lau <liuh01.cn@gmail.com>
2023-08-09 15:02:29 +0800

    fix 调整css布局

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Feedback from './feedback/page'
 export default function Home() {
 
   return (
-    <main className="flex flex-col min-h-screen items-center">
+    <main className="flex flex-col min-h-screen items-center justify-center">
       <Feedback/>
     </main>
   )
